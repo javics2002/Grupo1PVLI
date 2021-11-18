2cac460d00afb70be1b2611c2b7bf36ca6e5786c
javics2002 <49459590+javics2002@users.noreply.github.com>
2021-11-18 12:14:56 +0100

    Mapas

---
+++ assets/map/atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="atlas" tilewidth="32" tileheight="32" tilecount="26" columns="13">
+ <image source="atlas.png" width="416" height="64"/>
+</tileset>
