8c7fbdbaaf33186f4628a2f7b06fcbbe5e052e99
RuBeOX49 <61164466+RuBeOX49@users.noreply.github.com>
2021-11-18 17:14:44 +0100

    Update box.js

--- src/box.js
+++ src/box.js
@@ -1,1 +1,13 @@
-let weight;
+export default class Box extends Phaser.Physics.Matter.Sprite {
+    /**
+     * 
+     * @param {Phaser.Scene} scene Escena en la que aparece la caja
+     * @param {Number} x Coordenada X
+     * @param {Number} y Coordenada Y
+     * @param {Text} img Textura que determina la forma y el tamaño de la caja
+     */
+    constructor(scene, x, y, img){
+        super(scene.matter.world, x, y, img);
+        this.scene.add.existing(this);
+    }
+}
