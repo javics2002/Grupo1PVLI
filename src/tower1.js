ea0a5f948a418cd48306ecd9051c71f513275460
javics2002 <49459590+javics2002@users.noreply.github.com>
2021-11-24 21:14:13 +0100

    Espero arreglar merge

--- src/tower1.js
+++ src/tower1.js
@@ -16,5 +16,12 @@ export default class Tower1 extends Tower {
 
   update(t, dt) {
     super.update(t, dt);
+    
+    this.cameraRanges.forEach(element => {
+      if(this.player.y >= element.min && this.player.y < element.max){
+          this.cameras.main.setBounds(0, element.min, 1280, element.max + element.min);
+          this.cameras.main.startFollow(this.player);
+      }
+    });
   }
 }
