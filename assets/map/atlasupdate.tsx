4018249e1a55c982f6ef65d01080fed3aa04dc1c
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2021-12-09 18:10:09 +0100

    Create atlasupdate.tsx

---
+++ assets/map/atlasupdate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="atlasupdate" tilewidth="32" tileheight="32" tilecount="39" columns="13">
+ <image source="../../../../../Desktop/atlasupdate.png" width="416" height="96"/>
+</tileset>
